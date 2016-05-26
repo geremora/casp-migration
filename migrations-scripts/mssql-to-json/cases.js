30048c082c6d766a25f18c12ccc9e6b5adb29eaf
Gabriel Rivera Per-ossenkopp <gabriel.rivera2192@gmail.com>
2016-05-26 01:33:47 -0400

    fixing cases mapping, still needs work

--- migrations-scripts/mssql-to-json/cases.js
+++ migrations-scripts/mssql-to-json/cases.js
@@ -4,7 +4,9 @@ var async = require('async');
 var jsonfile = require('jsonfile');
 
 const migrationFile = __dirname + "/../migrations/cases_case.json";
-const CASES_OFFSET_ID = require('../constants/cases-constants.js').CASES_OFFSETS_ID;
+const CASES_OFFSET_ID = require('../constants/cases-constants.').CASES_OFFSETS_ID;
+const CASES_TYPE = require('../constants/cases-constants').CASES_TYPE;
+const CONTACT_OFFSET_ID = require('../constants/contacts-constants').CONTACT_OFFSETS_ID;
 
 module.exports = function(callback) {
     async.series({
@@ -15,33 +17,30 @@ module.exports = function(callback) {
                     objCase['id'] = radicaciones.RadicacionId + CASES_OFFSET_ID.OFFSET_TBL_RADICACIONES;
                     objCase['state'] = radicaciones.Pais == null ? "PR" : radicaciones.Pais;
                     objCase['container_id'] = radicaciones.RadicacionId + CASES_OFFSET_ID.OFFSET_TBL_CONTAINER;
-                    objCase['case_type_id'] = radicaciones.InActivo ? true : false;
+                    objCase['case_type_id'] = radicaciones.InActivo ? CASES_TYPE.UNASIGNED : CASES_TYPE.ARBITRAJE;
                     objCase['number'] = radicaciones.NumCaso;
-                    objCase['description'] = radicaciones.InformeOficial == null ? "No Description" : radicaciones.InformeOficial;
+                    objCase['description'] = radicaciones.InformeOficial;
                     objCase['date_created'] = radicaciones.FRegistrado;
                     objCase['date_updated'] = radicaciones.FAccesado;//CASE_CONTAINER.date_updated == null ? "1999-01-08" : CASE_CONTAINER.date_updated;
                     objCase['date_accepted'] = radicaciones.FRadicado == null ? "1999-01-08" : radicaciones.FRadicado;
                     objCase['created_by_id'] = radicaciones.UsuarioId;
-                    objCase['defendant_id'] = radicaciones.UsuarioId + CONTACT_TYPES.ABOGADO_REPRESENTANTE;
-                    objCase['plaintiff_id'] = radicaciones.UsuarioId + CONTACT_TYPES.EMPLEADO;
-                    objCase['assigned_user_id'] = radicaciones.UsuarioId;
+                    objCase['defendant_id'] = radicaciones.AgenciaId + CONTACT_OFFSET_ID.OFFSET_TBL_AGENCIAS;
+                    objCase['plaintiff_id'] = radicaciones.UsuarioId;
+                    objCase['assigned_user_id'] = radicaciones.OficialExaminador;
                     objCase['case_category_id'] = {
-                        id: radicaciones.UsuarioId,
                         name: radicaciones.NombreCaso,
                         description: radicaciones.ComentarioOficial
                     };
                     objCase['extra'] = "";
-                    objCase['did_confirm_case_type'] = "false";
-                    objCase['record_holder_id'] = "";
+                    objCase['did_confirm_case_type'] = false;
+                    objCase['record_holder_id'] = radicaciones.OficialExaminador;
                     return objCase;
                 });
                 return cb(null, pgCases);
             }).catch(function(error) {
                 return cb(error);
             });
-        },
-
-        
+        }
     }, function(error, results) {
         if(error) {
             return callback(error);
